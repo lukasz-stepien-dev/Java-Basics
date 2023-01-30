9d89cc6ddb3e17d3ab711ee9ad515739e935619c
Łukasz Stępień <lukaszatepien@gmail.com>
2023-01-30 20:19:27 +0100

    Create Math.java and add some calculations

---
+++ Math.java
@@ -0,0 +1,12 @@
+import static java.lang.Math.*;
+public class Math {
+    public static void main(String[] args)
+    {
+        int x = 9;
+        System.out.println(x + " to the second power equal " + pow(x, 2));
+        System.out.println("Square root of " + x + " equal " + sqrt(x));
+        System.out.println("Logarithm of " + x + " equal " + log(x));
+        System.out.println("Pi = " + PI);
+        System.out.println("E = " + E);
+    }
+}
