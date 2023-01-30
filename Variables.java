f4e42a6f1c1871c6dc9704ffc97f0f6651e734d0
Łukasz Stępień <lukaszatepien@gmail.com>
2023-01-30 20:07:31 +0100

    Add variable types

--- Variables.java
+++ Variables.java
@@ -1,7 +1,12 @@
 public class Variables {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+        int fullNumber = 100;
+        double realNumber = 1.75;
+        char letter = 'A';
+        boolean done = true;
+        String text = "Hello World";
+        final double price = 60.5; // const
     }
 
 }
