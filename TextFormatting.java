802a2b0992e909a3fcb9d53c44d8db7dbc19d0fe
Łukasz Stępień <lukaszatepien@gmail.com>
2023-02-01 19:47:06 +0100

    Add example text formatting with printf

--- TextFormatting.java
+++ TextFormatting.java
@@ -2,6 +2,12 @@ import java.lang.*;
 public class TextFormatting {
     public static void main(String[] args)
     {
-
+        String name = "Łukasz";
+        int age = 20;
+        String product = "t-shirt";
+        double price = 6.50;
+        String helloWorld = String.format("Hello %s!", name);
+        System.out.printf(helloWorld);
+        System.out.printf("%s buy %s for %,.2f and he is %d years old", name, product, price, age);
     }
 }
